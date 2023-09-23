fa8d310577240d76a0eeed5bc610e0140bc6de6c
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2023-09-23 14:23:42 +0300

    extracted to LangExtractor file

--- src/Console/LangExtractCommand.php
+++ src/Console/LangExtractCommand.php
@@ -3,8 +3,7 @@
 namespace Redot\LaravelLangExtractor\Console;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
-use Symfony\Component\Finder\Finder;
+use Redot\LaravelLangExtractor\LangExtractor;
 
 class LangExtractCommand extends Command
 {
@@ -23,86 +22,16 @@ class LangExtractCommand extends Command
     protected $description = 'Extract translation strings from blade files';
 
     /**
-     * Pattern to match translation strings.
-     */
-    protected string $pattern;
-
-    /**
-     * Create a new command instance.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->pattern = $this->getPatternFrom('__', 'trans', '@lang');
-    }
-
-    /**
-     * Get the pattern to match translation strings.
-     */
-    protected function getPatternFrom(string ...$functions): string
-    {
-        $ignore = glob(lang_path(config('app.fallback_locale')).'/*.php');
-        $ignore = array_map(fn ($file) => basename($file, '.php').'\.[^\s]', $ignore);
-
-        return '/(?:'.implode('|', $functions).")\((['\"])(?<translation>(?!".implode('|', $ignore).")(?:[^']|\\\')+?)(?<!\\\\)\\1/s";
-    }
-
-    /**
      * Execute the console command.
      */
     public function handle(): void
     {
         $language = $this->argument('language');
+        $path = lang_path($language . '.json');
 
-        $translations = $this->getTranslations();
+        $extractor = new LangExtractor();
+        $extractor->extract()->mergeWithFile($path)->save($path, true);
 
-        $this->saveTranslations($language, $translations);
-    }
-
-    /**
-     * Get all translations from blade files.
-     */
-    protected function getTranslations(): array
-    {
-        $translations = collect();
-
-        $files = Finder::create()->files()->ignoreVCSIgnored(true);
-        $files->in(resource_path())->in(app_path('Http'))->in(app_path('Livewire'))->name('*.php');
-
-        foreach ($files as $file) {
-            preg_match_all($this->pattern, $file->getContents(), $matches);
-
-            $translations = $translations->merge($matches['translation']);
-        }
-
-        $replacements = ['\"' => '"', '\\\'' => '\''];
-        $translations = $translations->map(fn ($translation) => trim(strtr($translation, $replacements)));
-
-        return $translations->filter()->unique()->values()->toArray();
-    }
-
-    /**
-     * Save translations to a language file.
-     */
-    protected function saveTranslations(string $language, array $translations): void
-    {
-        $path = lang_path($language.'.json');
-
-        $translations = array_combine($translations, $translations);
-        $old = File::exists($path) ? json_decode(File::get($path), true) : [];
-
-        if (count($old) > 0) {
-            $translations = array_merge($translations, $old);
-
-            File::delete($path);
-        }
-
-        ksort($translations);
-
-        $flags = JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
-        File::put($path, json_encode($translations, $flags));
-
-        $this->info('Translations saved to '.str_replace(base_path(), '', $path));
+        $this->info('Translations saved to ' . str_replace(base_path(), '', $path));
     }
 }
---
+++ src/LangExtractor.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Redot\LaravelLangExtractor;
+
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Finder\Finder;
+
+class LangExtractor
+{
+    /**
+     * Pattern to match translation strings.
+     */
+    protected string $pattern;
+
+    /**
+     * Directories to search for blade files.
+     */
+    protected array $directories = [];
+
+    /**
+     * Translations extracted from blade files.
+     */
+    protected array $translations = [];
+
+    /**
+     * Create a new instance.
+     */
+    public function __construct($directories = [])
+    {
+        $this->directories = $directories;
+
+        if (count($this->directories) === 0) {
+            $this->searchIn(resource_path(), app_path('Http'), app_path('Livewire'));
+        }
+
+        $this->pattern = $this->generatePatternUsing('__', 'trans', '@lang');
+    }
+
+    /**
+     * Set the directories to search for blade files.
+     */
+    public function searchIn(string ...$directories): static
+    {
+        $this->directories = $directories;
+
+        return $this;
+    }
+
+    /**
+     * Get the pattern to match translation strings.
+     */
+    protected function generatePatternUsing(string ...$functions): string
+    {
+        $ignore = glob(lang_path(config('app.fallback_locale')) . '/*.php');
+        $ignore = array_map(fn ($file) => basename($file, '.php') . '\.[^\s]', $ignore);
+
+        return '/(?:' . implode('|', $functions) . ")\((['\"])(?<translation>(?!" . implode('|', $ignore) . ")(?:[^']|\\\')+?)(?<!\\\\)\\1/s";
+    }
+
+    /**
+     * Get all translations from blade files.
+     */
+    public function extract(): static
+    {
+        $translations = collect();
+
+        $files = Finder::create()->files()->ignoreVCSIgnored(true);
+        $files->in($this->directories)->name('*.php');
+
+        foreach ($files as $file) {
+            preg_match_all($this->pattern, $file->getContents(), $matches);
+
+            $translations = $translations->merge($matches['translation']);
+        }
+
+        $replacements = ['\"' => '"', '\\\'' => '\''];
+        $translations = $translations->map(fn ($translation) => trim(strtr($translation, $replacements)));
+
+        $this->translations = $translations->filter()->unique()->values()->toArray();
+        $this->translations = array_combine($this->translations, $this->translations);
+
+        return $this;
+    }
+
+    /**
+     * Merge translations with existing translations.
+     */
+    public function mergeWithFile(string $path): static
+    {
+        $old = File::exists($path) ? json_decode(File::get($path), true) : [];
+
+        if (count($old) > 0) {
+            $this->translations = array_merge($this->translations, $old);
+            ksort($this->translations);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Merge translations with existing translations.
+     */
+    public function mergeWithArray(array $translations): static
+    {
+        $this->translations = array_merge($this->translations, $translations);
+        ksort($this->translations);
+
+        return $this;
+    }
+
+    /**
+     * Get all translations.
+     */
+    public function all(): array
+    {
+        return $this->translations;
+    }
+
+    /**
+     * Save translations to a language file.
+     */
+    public function save(string $path, bool $force = false): int|bool
+    {
+        if (File::exists($path) && ! $force) {
+            return false;
+        }
+
+        File::delete($path);
+
+        $flags = JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
+
+        return File::put($path, json_encode($this->translations, $flags));
+    }
+}
